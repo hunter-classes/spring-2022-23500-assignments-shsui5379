bf6568da10d27e53cafb93d2e78fe51e9f924de0
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-03-30 23:40:01 -0400

    solved

--- assignments/pointer-maze/Main.cpp
+++ assignments/pointer-maze/Main.cpp
@@ -9,42 +9,48 @@
  * do NOT edit the value of kYourName. Changing kYourName will change which
  * maze you get back, which might invalidate all your hard work!
  */
-const std::string kYourName = "TODO: Replace this string with your name.";
+const std::string kYourName = "Steven Hsui";
 
 /* Change these constants to contain the paths out of your mazes. */
-const std::string kPathOutOfNormalMaze = "TODO: Replace this string with your path out of the normal maze.";
+const std::string kPathOutOfNormalMaze = "WWWSSSENSWNNEESSENN";
 const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
 
-int main() {
+int main()
+{
     /* Generate the maze.
      *
      * Note: Don't set a breakpoint on this line. Otherwise, you'll see startLocation before
      * it's been initialized.
      */
-    MazeCell* startLocation = mazeFor(kYourName);
-    
+    MazeCell *startLocation = mazeFor(kYourName);
+
     /* Set a breakpoint here to explore your maze! */
-    if (isPathToFreedom(startLocation, kPathOutOfNormalMaze)) {
+    if (isPathToFreedom(startLocation, kPathOutOfNormalMaze))
+    {
         std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
     }
-    
-    
+
     /* Generate the twisty maze.
      *
      * Note: Don't set a breakpoint on this line. Otherwise, you'll see twistyStartLocation before
      * it's been initialized.
      */
-    MazeCell* twistyStartLocation = twistyMazeFor(kYourName);
-    
+    MazeCell *twistyStartLocation = twistyMazeFor(kYourName);
+
     /* Set a breakpoint here to explore your twisty maze! */
-    
-    if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze)) {
+
+    if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze))
+    {
         std::cout << "Congratulations! You've found a way out of your twisty labyrinth." << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Sorry, but you're still stuck in your twisty labyrinth." << std::endl;
     }
-    
+
     return 0;
 }
