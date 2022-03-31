9cb94de47750f0a6dea2dd9b51eb778ccad85440
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-03-30 23:39:45 -0400

    implement isPathToFreedom

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -1,8 +1,82 @@
 #include "Labyrinth.h"
 
-bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+bool isPathToFreedom(MazeCell *start, const std::string &moves)
+{
+    bool hasSpellbook = false;
+    bool hasPotion = false;
+    bool hasWand = false;
+
+    MazeCell *current = start;
+
+    if (current->whatsHere == Item::POTION)
+        hasPotion = true;
+    else if (current->whatsHere == Item::SPELLBOOK)
+        hasSpellbook = true;
+    else if (current->whatsHere == Item::WAND)
+        hasWand = true;
+
+    for (char move : moves)
+    {
+        switch (move)
+        {
+        case 'N':
+            current = current->north;
+
+            if (current == nullptr)
+                return false;
+
+            if (current->whatsHere == Item::POTION)
+                hasPotion = true;
+            else if (current->whatsHere == Item::SPELLBOOK)
+                hasSpellbook = true;
+            else if (current->whatsHere == Item::WAND)
+                hasWand = true;
+            break;
+
+        case 'E':
+            current = current->east;
+
+            if (current == nullptr)
+                return false;
+
+            if (current->whatsHere == Item::POTION)
+                hasPotion = true;
+            else if (current->whatsHere == Item::SPELLBOOK)
+                hasSpellbook = true;
+            else if (current->whatsHere == Item::WAND)
+                hasWand = true;
+
+            break;
+        case 'S':
+            current = current->south;
+
+            if (current == nullptr)
+                return false;
+
+            if (current->whatsHere == Item::POTION)
+                hasPotion = true;
+            else if (current->whatsHere == Item::SPELLBOOK)
+                hasSpellbook = true;
+            else if (current->whatsHere == Item::WAND)
+                hasWand = true;
+
+            break;
+        case 'W':
+            current = current->west;
+
+            if (current == nullptr)
+                return false;
+
+            if (current->whatsHere == Item::POTION)
+                hasPotion = true;
+            else if (current->whatsHere == Item::SPELLBOOK)
+                hasSpellbook = true;
+            else if (current->whatsHere == Item::WAND)
+                hasWand = true;
+
+            break;
+        }
+    }
+
+    return hasPotion && hasSpellbook && hasWand;
 }
