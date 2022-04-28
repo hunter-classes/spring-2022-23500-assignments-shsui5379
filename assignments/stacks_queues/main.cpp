7acbafb08bb8ad9c8ab3b3525d9f43dce9c0260a
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-04-28 01:53:13 -0400

    queue main

--- assignments/stacks_queues/main.cpp
+++ assignments/stacks_queues/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Stack.h"
+#include "Queue.h"
 
 int main()
 {
@@ -54,7 +55,66 @@ int main()
       std::cout << "Stack is empty" << std::endl;
    }
 
-   std::cout << "----------" << std::endl;
+   std::cout << "----------\nQUEUE" << std::endl;
+
+   Queue *queue = new Queue(3);
+   try
+   {
+      Queue *q = new Queue(0);
+   }
+   catch (int e)
+   {
+      std::cout << "0 is an invalid size" << std::endl;
+   }
+
+   std::cout << "is empty?: " << queue->is_empty() << std::endl;
+   std::cout << "is full?: " << queue->is_full() << std::endl;
+
+   queue->enqueue(1);
+   queue->enqueue(2);
+   queue->enqueue(3);
+
+   std::cout << "Queued 1, 2, 3" << std::endl;
+
+   std::cout << "Next up: " << queue->front() << std::endl;
+
+   std::cout << "is empty?: " << queue->is_empty() << std::endl;
+   std::cout << "is full?: " << queue->is_full() << std::endl;
+
+   try
+   {
+      queue->enqueue(4);
+   }
+   catch (int e)
+   {
+      std::cout << "Queue is full" << std::endl;
+   }
+
+   std::cout << "dequeue " << queue->dequeue() << std::endl;
+   std::cout << "Next up: " << queue->front() << std::endl;
+
+   std::cout << "enqueue 4" << std::endl;
+   queue->enqueue(4);
+   std::cout << "Next up: " << queue->front() << std::endl;
+
+   std::cout << "dequeue " << queue->dequeue() << std::endl;
+   std::cout << "Next up: " << queue->front() << std::endl;
+
+   std::cout << "dequeue " << queue->dequeue() << std::endl;
+   std::cout << "Next up: " << queue->front() << std::endl;
+
+   std::cout << "dequeue " << queue->dequeue() << std::endl;
+   try
+   {
+      queue->front();
+   }
+   catch (int e)
+   {
+      std::cout << "Queue is empty" << std::endl;
+   }
+
+   std::cout << "is empty?: " << queue->is_empty() << std::endl;
+   std::cout << "is full?: " << queue->is_full() << std::endl;
 
    return 0;
 }
