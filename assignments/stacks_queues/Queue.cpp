1ac39bca4d65f2264c0112c615e4b782c22c73b9
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-04-28 01:15:42 -0400

    is full, is empty, enqueue

--- assignments/stacks_queues/Queue.cpp
+++ assignments/stacks_queues/Queue.cpp
@@ -15,7 +15,6 @@ Queue::Queue(int s)
    head = 0;
    tail = 0;
    arr = new int[s];
-   empty = true;
    size = s;
 }
 
@@ -25,4 +24,40 @@ Queue::Queue(int s)
 Queue::~Queue()
 {
    delete[] arr;
+}
+
+/**
+ * Adds an item to this Queue
+ *
+ * @param n  The item to add
+ * */
+void Queue::enqueue(int n)
+{
+   if (is_full())
+   {
+      throw QUEUE_ERR_FULL;
+   }
+
+   tail++;
+   arr[tail % size] = n;
+}
+
+/**
+ * Determine if this Queue is full
+ *
+ * @returns  True if full
+ * */
+bool Queue::is_full()
+{
+   return (tail + 1) % size == head % size;
+}
+
+/**
+ * Determiens if this Queue is empty
+ *
+ * @returns  True if empty
+ * */
+bool Queue::is_empty()
+{
+   return head % size == tail % size;
 }
