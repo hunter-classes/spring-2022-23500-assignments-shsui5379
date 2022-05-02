7f1b298b20ecfaaca29607c7f628ed692671cd91
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-01 21:25:33 -0400

    rsearch test

--- assignments/trees/tests.cpp
+++ assignments/trees/tests.cpp
@@ -4,3 +4,18 @@
 
 #include "Node.h"
 #include "BSTree.h"
+
+TEST_CASE("rsearch")
+{
+   BSTree *tree = new BSTree();
+   tree->setup();
+
+   CHECK(tree->rsearch(3) == 3);
+   CHECK(tree->rsearch(5) == 5);
+   CHECK(tree->rsearch(8) == 8);
+   CHECK(tree->rsearch(15) == 15);
+   CHECK(tree->rsearch(20) == 20);
+   CHECK(tree->rsearch(30) == 30);
+
+   CHECK_THROWS(tree->rsearch(0), 1);
+}
