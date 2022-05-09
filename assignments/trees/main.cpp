46afc828de0bffa2ec1bb9b4e1135db3ba73428f
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-08 22:31:11 -0400

    main for remove

--- assignments/trees/main.cpp
+++ assignments/trees/main.cpp
@@ -28,5 +28,88 @@ int main()
    std::cout << tree->rsearch(17) << std::endl;
    std::cout << tree->get_debug_string() << std::endl;
 
+   std::cout << "----------" << std::endl;
+
+   BSTree *tree2 = new BSTree();
+   tree2->setup();
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(10);
+   try
+   {
+      tree2->rsearch(10);
+   }
+   catch (int e)
+   {
+      std::cout << "10 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(20);
+   try
+   {
+      tree2->rsearch(20);
+   }
+   catch (int e)
+   {
+      std::cout << "20 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(30);
+   try
+   {
+      tree2->rsearch(30);
+   }
+   catch (int e)
+   {
+      std::cout << "30 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(5);
+   try
+   {
+      tree2->rsearch(5);
+   }
+   catch (int e)
+   {
+      std::cout << "5 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(15);
+   try
+   {
+      tree2->rsearch(15);
+   }
+   catch (int e)
+   {
+      std::cout << "15 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(8);
+   try
+   {
+      tree2->rsearch(8);
+   }
+   catch (int e)
+   {
+      std::cout << "8 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
+   tree2->remove(3);
+   try
+   {
+      tree2->rsearch(3);
+   }
+   catch (int e)
+   {
+      std::cout << "3 got removed" << std::endl;
+   }
+   std::cout << tree2->get_debug_string() << std::endl;
+
    return 0;
 }
