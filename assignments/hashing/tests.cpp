ce4dd2c226494c2a8ad61c8afa32c3e0c15d72af
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-17 23:43:43 -0400

    tests for hash

--- assignments/hashing/tests.cpp
+++ assignments/hashing/tests.cpp
@@ -9,3 +9,10 @@ TEST_CASE("valid size")
    CHECK_NOTHROW(new Dictionary(1));
    CHECK_THROWS(new Dictionary(0), INVALID_SIZE);
 }
+
+TEST_CASE("hash")
+{
+   Dictionary *d = new Dictionary(3);
+   CHECK(d->hash("hash") == ('h' + 'a' + 's' + 'h') % 3);
+   CHECK(d->hash("") == 0);
+}
