1b322eabc33a6358226209857faa17e89e5cdca7
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-17 23:39:56 -0400

    valid size tests

--- assignments/hashing/tests.cpp
+++ assignments/hashing/tests.cpp
@@ -3,3 +3,9 @@
 #include "../../commons/doctest.h"
 
 #include "Dictionary.h"
+
+TEST_CASE("valid size")
+{
+   CHECK_NOTHROW(new Dictionary(1));
+   CHECK_THROWS(new Dictionary(0), INVALID_SIZE);
+}
