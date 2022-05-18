f8171e639a40d2a4f548f1c3fc9740c2d8b358b2
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-17 23:12:23 -0400

    Dictionary constructor and destructor

--- assignments/hashing/Dictionary.cpp
+++ assignments/hashing/Dictionary.cpp
@@ -1,1 +1,38 @@
 #include "Dictionary.h"
+
+/**
+ * @brief Construct a new Dictionary:: Dictionary object
+ *
+ * @param s  The size of the Dictionary
+ */
+Dictionary::Dictionary(int s)
+{
+   if (s < 1)
+   {
+      throw INVALID_SIZE;
+   }
+
+   size = s;
+   arr = new List *[size];
+
+   for (int i = 0; i < size; i++)
+   {
+      arr[i] = new List();
+   }
+}
+
+/**
+ * @brief Destroy the Dictionary:: Dictionary object
+ *
+ */
+Dictionary::~Dictionary()
+{
+   for (int i = 0; i < size; i++)
+   {
+      delete arr[i];
+      arr[i] = nullptr;
+   }
+
+   delete[] arr;
+   arr = nullptr;
+}
--- assignments/hashing/Dictionary.h
+++ assignments/hashing/Dictionary.h
@@ -2,8 +2,15 @@
 
 #include "List.h"
 
+#define INVALID_SIZE 1
+
 class Dictionary
 {
 private:
+   List **arr;
+   int size;
+
 public:
+   Dictionary(int s);
+   ~Dictionary();
 };
