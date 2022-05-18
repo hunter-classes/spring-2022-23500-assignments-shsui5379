a3804b3bc47f9766e1d41660a28b36e513f99db5
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-05-18 01:21:54 -0400

    main

--- assignments/hashing/main.cpp
+++ assignments/hashing/main.cpp
@@ -3,6 +3,28 @@
 
 int main()
 {
+   try
+   {
+      Dictionary *d = new Dictionary(0);
+   }
+   catch (int e)
+   {
+      std::cout << "Dictionary must have a size of at least 1" << std ::endl;
+   }
+
+   Dictionary *d = new Dictionary(3);
+
+   d->insert("Shalom", "Tomczak");
+   d->insert("Kibwe", "Sampson");
+   d->insert("Aitor", "Espensen");
+   d->insert("Karma", "Yamagishi");
+   d->insert("Lena", "Jans");
+   d->insert("Aroa", "MacKenna");
+
+   std::cout << d->getAll() << std::endl;
+
+   std::cout << d->get("Lena", "Jans")->get_name() << " exists" << std::endl;
+   std::cout << "Aroa Jans" << ((d->get("Aroa", "Jans") == nullptr) ? " doesn't" : "does") << std::endl;
 
    return 0;
 }
